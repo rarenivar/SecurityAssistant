c97f90e5bb774369e7c549b418491964bdb2da34
Ramiro Arenivar <ramiro@rarenivar.com>
2017-11-22 18:58:32 -0700

    Added settings page with UI - using a preferencefragment

--- app/src/main/java/com/rarenivar/securityassistant/views/SettingsActivity.java
+++ app/src/main/java/com/rarenivar/securityassistant/views/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.rarenivar.securityassistant.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 import com.rarenivar.securityassistant.R;
 
@@ -11,5 +13,24 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        // Insert the settings fragment in the FrameLayout we added earlier
+        getFragmentManager().beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit();
+        //setContentView(R.layout.activity_settings);
+    }
+
+    public static class SettingsFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
